bc94245fa3c6274db1e9d416d3b6ba129c815e14
Matthew McDermott <mattmcdermott8@gmail.com>
2019-05-06 10:51:48 -0400

    Added codestatus to statics, and switched back to categories (where appropriate) as it turns out that wasn't the bug.

--- SQL_Queries/statics.sql
+++ SQL_Queries/statics.sql
@@ -19,10 +19,16 @@ select distinct
     a.discharge_location,
     CASE when a.deathtime between i.intime and i.outtime THEN 1 ELSE 0 END AS mort_icu,
     CASE when a.deathtime between i.admittime and i.dischtime THEN 1 ELSE 0 END AS mort_hosp,
-    s.first_careunit
+    s.first_careunit,
+    c.fullcode_first,
+    c.dnr_first,
+    c.fullcode,
+    c.dnr,
+    c.timednr_chart
 FROM icustay_detail i
     INNER JOIN admissions a ON i.hadm_id = a.hadm_id
     INNER JOIN icustays s ON i.icustay_id = s.icustay_id
+    INNER JOIN code_status c ON i.icustay_id = c.icustay_id
 WHERE s.first_careunit NOT like 'NICU'
     and i.hadm_id is not null and i.icustay_id is not null
     and i.hospstay_seq = 1
