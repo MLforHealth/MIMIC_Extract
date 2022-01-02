c077df95db9e053e6996dbae25389708d3feef93
Julian Gilbey <julian-git@d-and-j.net>
2022-01-02 07:48:54 +0000

    Fix local settings facility: only perform actions once local settings read

--- utils/setup_user_env.sh
+++ utils/setup_user_env.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 export MIMIC_CODE_DIR=$(realpath ../../mimic-code)
-export MIMIC_EXTRACT_CODE_DIR=$(realpath ../)
 
-export MIMIC_DATA_DIR=$MIMIC_EXTRACT_CODE_DIR/data/
-
-export MIMIC_EXTRACT_OUTPUT_DIR=$MIMIC_DATA_DIR/curated/
-mkdir -p $MIMIC_EXTRACT_OUTPUT_DIR
+export MIMIC_EXTRACT_CODE_DIR=$(realpath ..)
+export MIMIC_EXTRACT_OUTPUT_DIR=$MIMIC_EXTRACT_CODE_DIR/data/curated
 
 export DBUSER=mimic
 export DBNAME=mimic
@@ -15,10 +12,13 @@ export SCHEMA=mimiciii
 export HOST=SOCKET
 export PORT=5432
 
+# Allow users to override any of the above in a local configuration file
 if [ -f "setup_user_env_local.sh" ]
 then
     . setup_user_env_local.sh
 fi
+
+mkdir -p $MIMIC_EXTRACT_OUTPUT_DIR
 
 if [ $HOST = SOCKET ]
 then
