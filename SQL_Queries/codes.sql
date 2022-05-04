10dffa452f0627fcba90b83c61288e700ff44105
minkoo <mit2400@naver.com>
2022-05-04 09:32:00 +0900

    Use search path instead of dot notation

--- SQL_Queries/codes.sql
+++ SQL_Queries/codes.sql
@@ -1,8 +1,9 @@
+SET SEARCH_PATH TO 'public,mimiciii';
 SELECT
     i.icustay_id, d.subject_id, d.hadm_id,
     array_agg(d.icd9_code ORDER BY seq_num ASC) AS icd9_codes
-FROM mimiciii.diagnoses_icd d 
-    LEFT OUTER JOIN (SELECT ccs_matched_id, icd9_code from public.ccs_dx) c
+FROM diagnoses_icd d 
+    LEFT OUTER JOIN (SELECT ccs_matched_id, icd9_code from ccs_dx) c
     ON c.icd9_code = d.icd9_code
     INNER JOIN icustays i
     ON i.hadm_id = d.hadm_id AND i.subject_id = d.subject_id
